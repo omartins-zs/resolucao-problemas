5dfb10c79298497f628795e28033f13ef9b02a74
Gabriel Martins <gabrielmatheus209@gmail.com>
2021-08-30 15:38:42 -0300

    Usando push e condicao senão

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
-let listaDeEstudantes = ["Helena", "Chico", "Mário"];
+let listaDeEstudantes = ["Helena", "Chico", "Mário", "José", "Maria"];
 let quantidadeDeEstudantes = listaDeEstudantes.length;
 if (quantidadeDeEstudantes < 5) {
-  listaDeEstudantes.push("José");
+  listaDeEstudantes.push("João");
+  console.log(listaDeEstudantes);
+} else {
+  console.log("Não é possível inserir mais alunos nessa turma");
 }
-console.log(listaDeEstudantes);
